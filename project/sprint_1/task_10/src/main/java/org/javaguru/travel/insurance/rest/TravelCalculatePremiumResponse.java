526de06e24529c807cfba79982a0449ec5cb2148
Yan <fave08@mail.ru>
2025-01-29 13:15:06 +0300

    Start project

--- project/sprint_1/task_10/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ project/sprint_1/task_10/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -9,4 +9,48 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateFrom;
     private Date agreementDateTo;
 
+    public TravelCalculatePremiumResponse() {}
+
+    public TravelCalculatePremiumResponse(String personFirstName,
+                                          String personLastName,
+                                          Date agreementDateFrom,
+                                          Date agreementDateTo) {
+        this.personFirstName = personFirstName;
+        this.personLastName = personLastName;
+        this.agreementDateFrom = agreementDateFrom;
+        this.agreementDateTo = agreementDateTo;
+    }
+
+    private String getPersonFirstName() {
+        return personFirstName;
+    }
+
+    private void setPersonFirstName(String personFirstName) {
+    this.personFirstName = personFirstName;
+    }
+
+    private String getPersonLastName() {
+        return personLastName;
+    }
+
+    private void setPersonLastName(String personLastName) {
+        this.personLastName = personLastName;
+    }
+
+    private Date getAgreementDateFrom() {
+        return agreementDateFrom;
+    }
+
+    private void setAgreementDateFrom(Date agreementDateFrom) {
+        this.agreementDateFrom = agreementDateFrom;
+    }
+
+    private Date getAgreementDateTo() {
+        return agreementDateTo;
+    }
+
+    private void setAgreementDateTo(Date agreementDateTo) {
+        this.agreementDateTo = agreementDateTo;
+    }
 }
+
